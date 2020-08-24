22735c8c35e3c9ad006e7b82c3110dbd1e52ae01
Sepehrkianian09 <sepehrkianian09@yahoo.com>
2020-08-24 04:53:47 +0430

    Add Program.cs

---
+++ AlphaApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlphaApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
